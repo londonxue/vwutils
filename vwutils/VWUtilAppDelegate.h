4d6b1d7ab48b452dc1072f9d48febfcd1d5288f1
london <londonxue@163.com>
2015-10-27 15:44:18 +0800

    dfd

---
+++ vwutils/VWUtilAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vwutils
+//
+//  Created by london xue on 27/10/15.
+//  Copyright (c) 2015 sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWUtilAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
